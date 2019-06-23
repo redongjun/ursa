d7987b9c421f0139b3ccf78b56c4d4eb9968993a
LEE Dongjun <redongjun@gmail.com>
2019-06-23 21:56:57 +0900

    Add the issuance group

--- libursa/benches/cks_revok.rs
+++ libursa/benches/cks_revok.rs
@@ -26,6 +26,14 @@ fn get_non_credential_schema() -> NonCredentialSchema {
     non_credential_schema_builder.finalize().unwrap()
 }
 
+fn get_credential_secret_value(master_secret: &MasterSecret) -> CredentialValues {
+    let mut credential_values_builder = Issuer::new_credential_values_builder().unwrap();
+    credential_values_builder
+        .add_value_known("master_secret", &master_secret.value().unwrap())
+        .unwrap();
+    credential_values_builder.finalize().unwrap()
+}
+
 fn get_credential_values(master_secret: &MasterSecret) -> CredentialValues {
     let mut credential_values_builder = Issuer::new_credential_values_builder().unwrap();
     credential_values_builder
@@ -55,6 +63,136 @@ fn get_sub_proof_request() -> SubProofRequest {
     sub_proof_request_builder.finalize().unwrap()
 }
 
+fn setup_cred(
+    max_cred_num: u32,
+    issuance_by_default: bool,
+) -> (
+    CredentialSchema,
+    NonCredentialSchema,
+    CredentialPublicKey,
+    CredentialPrivateKey,
+    CredentialKeyCorrectnessProof,
+    RevocationRegistry,
+    RevocationKeyPublic,
+    RevocationKeyPrivate,
+    SimpleTailsAccessor
+)
+{
+    let credential_schema = get_credential_schema();
+    let non_credential_schema = get_non_credential_schema();
+
+    // 2. Issuer creates credential definition(with revocation keys)
+    let (credential_pub_key, credential_priv_key, credential_key_correctness_proof) =
+        Issuer::new_credential_def(&credential_schema, &non_credential_schema, true).unwrap();
+
+    // 3. Issuer creates revocation registry
+    let (rev_key_pub, rev_key_priv, rev_reg, mut rev_tails_generator) =
+        Issuer::new_revocation_registry_def(&credential_pub_key, max_cred_num, issuance_by_default)
+        .unwrap();
+
+    let simple_tail_accessor = SimpleTailsAccessor::new(&mut rev_tails_generator).unwrap();
+
+    (
+        credential_schema,
+        non_credential_schema,
+        credential_pub_key,
+        credential_priv_key,
+        credential_key_correctness_proof,
+        rev_reg,
+        rev_key_pub,
+        rev_key_priv,
+        simple_tail_accessor,
+    )
+}
+
+fn get_blinded_credential(
+    credential_pub_key: &CredentialPublicKey,
+    credential_key_correctness_proof: &CredentialKeyCorrectnessProof
+) -> (
+    BlindedCredentialSecrets,
+    CredentialSecretsBlindingFactors,
+    BlindedCredentialSecretsCorrectnessProof,
+    Nonce,
+    CredentialValues,
+) {
+    let credential_secret_value = get_credential_values(&Prover::new_master_secret().unwrap());
+
+    // 5. Issuer creates nonce used by Prover to create correctness proof for blinded secrets
+    let blinding_correctness_nonce = new_nonce().unwrap();
+
+    // 6. Prover blinds master secret
+    let (
+        blinded_credential_secrets,
+        credential_secrets_blinding_factors,
+        blinded_credential_secrets_correctness_proof,
+    ) = Prover::blind_credential_secrets(
+        &credential_pub_key,
+        &credential_key_correctness_proof,
+        &credential_secret_value,
+        &blinding_correctness_nonce,
+    )
+        .unwrap();
+
+    (
+        blinded_credential_secrets,
+        credential_secrets_blinding_factors,
+        blinded_credential_secrets_correctness_proof,
+        blinding_correctness_nonce,
+        credential_secret_value,
+    )
+}
+
+fn gen_issuance(
+    blinded_credential_secrets: &BlindedCredentialSecrets,
+    blinded_credential_secrets_correctness_proof: &BlindedCredentialSecretsCorrectnessProof,
+    blinding_correctness_nonce: &Nonce,
+    signature_correctness_nonce: &Nonce,
+    credential_values: &CredentialValues,
+    credential_pub_key: &CredentialPublicKey,
+    credential_priv_key: &CredentialPrivateKey,
+    rev_idx: u32,
+    max_cred_num: u32,
+    issuance_by_default: bool,
+    rev_reg: &mut RevocationRegistry,
+    rev_key_priv: &RevocationKeyPrivate,
+    simple_tail_accessor: &SimpleTailsAccessor
+) -> (
+    CredentialSignature,
+    SignatureCorrectnessProof,
+    Option<RevocationRegistryDelta>,
+) {
+    // 7. Prover creates nonce used by Issuer to create correctness proof for signature
+    //let signature_correctness_nonce = new_nonce().unwrap();
+
+    //let credential_values = get_credential_values(&Prover::new_master_secret().unwrap());
+
+    // 8. Issuer creates and sign credential values
+    let (credential_signature, signature_correctness_proof, rr_delta) =
+        Issuer::sign_credential_with_revoc(
+            &rev_idx.to_string(),
+            blinded_credential_secrets,
+            blinded_credential_secrets_correctness_proof,
+            blinding_correctness_nonce,
+            signature_correctness_nonce,
+            credential_values,
+            credential_pub_key,
+            credential_priv_key,
+            rev_idx,
+            max_cred_num,
+            issuance_by_default,
+            rev_reg,
+            rev_key_priv,
+            simple_tail_accessor,
+        )
+        .unwrap();
+
+    (
+        credential_signature,
+        signature_correctness_proof,
+        rr_delta
+    )
+}
+
 fn setup_cred_and_issue(
     max_cred_num: u32,
     issuance_by_default: bool,
@@ -66,66 +204,56 @@ fn setup_cred_and_issue(
     RevocationRegistry,
     Vec<(CredentialValues, CredentialSignature, Witness)>,
 ) {
-    let credential_schema = get_credential_schema();
-    let non_credential_schema = get_non_credential_schema();
-
-    // 2. Issuer creates credential definition(with revocation keys)
-    let (credential_pub_key, credential_priv_key, credential_key_correctness_proof) =
-        Issuer::new_credential_def(&credential_schema, &non_credential_schema, true).unwrap();
-
-    // 3. Issuer creates revocation registry
-    let (rev_key_pub, rev_key_priv, mut rev_reg, mut rev_tails_generator) =
-        Issuer::new_revocation_registry_def(&credential_pub_key, max_cred_num, issuance_by_default)
-            .unwrap();
-
-    let simple_tail_accessor = SimpleTailsAccessor::new(&mut rev_tails_generator).unwrap();
-
+    let (
+        credential_schema,
+        non_credential_schema,
+        credential_pub_key,
+        credential_priv_key,
+        credential_key_correctness_proof,
+        mut rev_reg,
+        rev_key_pub,
+        rev_key_priv,
+        simple_tail_accessor,
+    ) = setup_cred(max_cred_num, issuance_by_default);
+    
     let mut _prover_data: Vec<(CredentialValues, CredentialSignature, Witness)> = vec![];
 
     let mut rev_reg_delta: Option<RevocationRegistryDelta> = None;
 
     for i in 0..max_cred_num {
-        let credential_values = get_credential_values(&Prover::new_master_secret().unwrap());
-
-        // 5. Issuer creates nonce used by Prover to create correctness proof for blinded secrets
-        let blinding_correctness_nonce = new_nonce().unwrap();
-
-        // 6. Prover blinds master secret
         let (
             blinded_credential_secrets,
             credential_secrets_blinding_factors,
             blinded_credential_secrets_correctness_proof,
-        ) = Prover::blind_credential_secrets(
+            blinding_correctness_nonce,
+            credential_values,
+        ) = get_blinded_credential(
             &credential_pub_key,
             &credential_key_correctness_proof,
-            &credential_values,
-            &blinding_correctness_nonce,
-        )
-        .unwrap();
-
+        );
+        
         // 7. Prover creates nonce used by Issuer to create correctness proof for signature
         let signature_correctness_nonce = new_nonce().unwrap();
 
+        //let credential_values = get_credential_values(&Prover::new_master_secret().unwrap());
+
         // 8. Issuer creates and sign credential values
         let rev_idx = i + 1;
-        let (mut credential_signature, signature_correctness_proof, rr_delta) =
-            Issuer::sign_credential_with_revoc(
-                &rev_idx.to_string(),
-                &blinded_credential_secrets,
-                &blinded_credential_secrets_correctness_proof,
-                &blinding_correctness_nonce,
-                &signature_correctness_nonce,
-                &credential_values,
-                &credential_pub_key,
-                &credential_priv_key,
-                rev_idx,
-                max_cred_num,
-                issuance_by_default,
-                &mut rev_reg,
-                &rev_key_priv,
-                &simple_tail_accessor,
-            )
-            .unwrap();
+        let (mut credential_signature, signature_correctness_proof, rr_delta) = gen_issuance(
+            &blinded_credential_secrets,
+            &blinded_credential_secrets_correctness_proof,
+            &blinding_correctness_nonce,
+            &signature_correctness_nonce,
+            &credential_values,
+            &credential_pub_key,
+            &credential_priv_key,
+            rev_idx,
+            max_cred_num,
+            issuance_by_default,
+            &mut rev_reg,
+            &rev_key_priv,
+            &simple_tail_accessor,
+        );
 
         if i == 0 {
             rev_reg_delta = rr_delta;
@@ -224,6 +352,67 @@ fn gen_proofs(
     proofs
 }
 
+fn bench_cks_issuance_revok_on_demand_issuance(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "cks revocation issuance generation with on demand issuance",
+        move |b, (max_cred_num, _num_proofs_to_do)| {
+            let max_cred_num = *max_cred_num as u32;
+            let issuance_by_default = false;
+
+            let (
+                _credential_schema,
+                _non_credential_schema,
+                credential_pub_key,
+                credential_priv_key,
+                credential_key_correctness_proof,
+                mut rev_reg,
+                _rev_key_pub,
+                rev_key_priv,
+                simple_tail_accessor,
+            ) = setup_cred(max_cred_num, issuance_by_default);
+
+            let (
+                blinded_credential_secrets,
+                _credential_secrets_blinding_factors,
+                blinded_credential_secrets_correctness_proof,
+                blinding_correctness_nonce,
+                credential_values,
+            ) = get_blinded_credential(
+                &credential_pub_key,
+                &credential_key_correctness_proof,
+            );
+            
+            // 7. Prover creates nonce used by Issuer to create correctness proof for signature
+            let signature_correctness_nonce = new_nonce().unwrap();
+
+            //let credential_values = get_credential_values(&Prover::new_master_secret().unwrap());
+
+            // 8. Issuer creates and sign credential values
+            let rev_idx = 1;
+
+            b.iter(|| {
+                gen_issuance(
+                    &blinded_credential_secrets,
+&blinded_credential_secrets_correctness_proof,
+                    &blinding_correctness_nonce,
+                    &signature_correctness_nonce,
+                    &credential_values,
+                    &credential_pub_key,
+                    &credential_priv_key,
+                    rev_idx,
+                    max_cred_num,
+                    issuance_by_default,
+                    &mut rev_reg,
+                    &rev_key_priv,
+                    &simple_tail_accessor,
+                );
+            });
+        },
+        //vec![(100, 10), (10000, 10)],
+        vec![(10, 2), (100, 2)],
+    );
+}
+
 fn bench_cks_prove_revok_on_demand_issuance(c: &mut Criterion) {
     c.bench_function_over_inputs(
         "cks revocation proof generation with on demand issuance",
@@ -318,6 +507,14 @@ fn bench_cks_verify_revok_on_demand_issuance(c: &mut Criterion) {
 }
 
 criterion_group! {
+    name = cks_issuance_revok_on_demand;
+    config = Criterion::default().sample_size(3);
+    //config = Criterion::default();
+    targets =
+        bench_cks_issuance_revok_on_demand_issuance,
+}
+
+criterion_group! {
     name = cks_prove_revok_on_demand;
     config = Criterion::default().sample_size(3);
     //config = Criterion::default();
@@ -333,4 +530,4 @@ criterion_group! {
     bench_cks_verify_revok_on_demand_issuance,
 }
 
-criterion_main!(cks_prove_revok_on_demand, cks_verify_revok_on_demand);
+criterion_main!(cks_issuance_revok_on_demand, cks_prove_revok_on_demand, cks_verify_revok_on_demand);
